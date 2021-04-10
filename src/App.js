25c055aff83d2c4713b10589a1103141e6154ce9
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-04-10 21:52:28 +0600

    Outputtinng Dynamic content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I am ReactApp!</h1>
+        <p>This is realy working. ()</p>
+        <Person />
+        <Person />
       </div>
+
     );
+
+    //return React.createElement('div' , {className:'App'}, React.createElement('h1' ,null ,'Does this work now ?'));
+
   }
 }
 
